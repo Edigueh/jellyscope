808dae4033c872a59531aeb230e3ebe0ddbda668
Edigueh <andreluck2003@gmail.com>
2026-05-26 21:05:28 -0300

    disable SED

--- src/jellyscope/config.py
+++ src/jellyscope/config.py
@@ -50,4 +50,4 @@ class JellyscopeConfig(BaseModel):
     debug: bool = True
     default_colorscale: str = "Viridis"
     filter_wavelengths: dict[str, float] = Field(default_factory=lambda: dict(NIRCAM_WAVELENGTHS))
-    enable_sed: bool = True
+    enable_sed: bool = False
